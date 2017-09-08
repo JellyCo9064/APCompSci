9201ca2fe4ccb487a895eab8a0a0a777f7d72e43
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2017-09-08 10:11:02 -0700

    9/8/2017

---
+++ 2_2Squares/src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int firstDif = 2;
+		int outputNum = 1;
+		
+		Scanner console = new Scanner(System.in);
+		
+		//COMMENT! From C dog cash money aksama
+		
+		for(int i = 0; i < 9; i++);
+		
+	}
+
+}
