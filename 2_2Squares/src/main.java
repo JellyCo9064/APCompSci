6ee2ef5d62508c5fc53202639bbe6cc4a8a11f00
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2017-09-08 10:18:31 -0700

    Finished 9/8/17

--- 2_2Squares/src/main.java
+++ 2_2Squares/src/main.java
@@ -4,14 +4,21 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int firstDif = 2;
+		int firstDif = 3;
 		int outputNum = 1;
 		
 		Scanner console = new Scanner(System.in);
 		
-		//COMMENT! From C dog cash money aksama
+		int desiredNum = console.nextInt();
 		
-		for(int i = 0; i < 9; i++);
+		console.close();
+		
+		for(int i = 1; i <= desiredNum; i++)
+		{
+			System.out.println(outputNum);
+			outputNum += firstDif;
+			firstDif += 2;
+		}
 		
 	}
 
