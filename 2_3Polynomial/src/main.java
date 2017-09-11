9ffbd085bfc46063daf9c9e5d451b386f9742347
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2017-09-11 09:48:47 -0700

    All Done 9/11

--- 2_3Polynomial/src/main.java
+++ 2_3Polynomial/src/main.java
@@ -1,10 +1,23 @@
-
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner console = new Scanner(System.in);
+		
 		System.out.println("Enter parameters of polynomial");
-		System.out.println("");
+		System.out.println("a: ");
+		double a = console.nextDouble();
+		System.out.println("b: ");
+		double b = console.nextDouble();
+		System.out.println("c: ");
+		double c = console.nextDouble();
+		
+		double[] roots = quadratic(a, b, c);
+		
+		System.out.println(roots[0] + " and " + roots[1]);
+		
 	}
 	
 	public static double[] quadratic(double a, double b, double c)
