3db187a0d23dfc42fa759a424ded5f9c96fef255
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2017-09-14 10:36:56 -0700

    Finished

--- 3_4Counter/src/main.java
+++ 3_4Counter/src/main.java
@@ -1,6 +1,12 @@
-import java.awt.List;
+/*
+ * Author: Connor Aksama
+ * Project Name: 3_4Counter
+ * File Name: main.java
+ * Purpose: Read files, check characters.
+ * Pseudocode: Read file line by line, scan each line
+ * Maintenance Log: 9/13 Began reading. 9/14 done
+ */
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main 
@@ -15,17 +21,36 @@ public class main
 			Scanner console = new Scanner(System.in);
 			System.out.println("Enter file name: ");
 			String fileName = console.nextLine();
-			
-			Scanner file = new Scanner(new File(fileName));
-			
-			ArrayList<String> fileLines = new ArrayList<String>();
-			
-			while(file.hasNextLine())
+			Scanner file = null;
+			try
 			{
-				fileLines.add(file.nextLine());
+				file = new Scanner(new File(fileName));
 			}
+			catch(FileNotFoundException e)
+			{
+				System.out.println("no");
+			}
+			int characters = 0;
+			int lines = 0;
+			int words = 0;
 			
-				
+			while (file.hasNextLine())
+			{
+				String test = file.nextLine();
+				lines++;
+				characters += test.length();
+				Scanner retest = new Scanner(test);
+				while (retest.hasNext())
+				{
+					words++;
+					retest.next();
+				}
+				retest.close();
+			}
+			file.close();
+			System.out.println("Characters: " + characters);
+			System.out.println("Lines: " + lines);
+			System.out.println("Words: " + words);
 	}
 
 }
