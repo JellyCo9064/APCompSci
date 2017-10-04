0604668d79130a55616cea875801d4f2c21366c2
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2017-10-04 10:30:59 -0700

    Activity One Done

---
+++ 6_3Elevens/src/CardTester.java
@@ -0,0 +1,17 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card card1 = new Card("Nine", "Hearts", 9);
+		Card card2 = new Card("Ten", "Clubs", 10);
+		Card card3 = new Card("Eight", "Diamonds", 8);
+		
+		System.out.println(card1.toString() + "\n" + card2.toString() + "\n" + card3.toString());
+	}
+}
