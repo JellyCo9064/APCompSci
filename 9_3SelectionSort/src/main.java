b6b70a1a06295cdeff3b674ce088acf0fecdb799
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2017-11-06 10:38:38 -0800

    Needs comments, answers to questions

---
+++ 9_3SelectionSort/src/main.java
@@ -0,0 +1,40 @@
+
+import java.lang.management.ManagementFactory;
+import java.util.*;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] moo = {20, 12, 10, 15, 2};
+		for(int i : moo)
+		{
+			System.out.print(i + ", ");
+		}
+		moo = sort(moo);
+		System.out.println();
+		for(int i : moo)
+		{
+			System.out.print(i + ", ");
+		}
+	}
+	public static int[] sort(int[] array)
+	{
+		for(int i = array.length - 1; i > 0; i--)
+		{
+			int greatest = Integer.MIN_VALUE;
+			int gIndex = 0;
+			for(int j = 0; j <= i; j++)
+			{
+				if (array[j] > greatest)
+				{
+					greatest = array[j];
+					gIndex = j;
+				}
+			}
+			int temp = array[i];
+			array[i] = greatest;
+			array[gIndex] = temp;
+		}
+		return array;
+	}
+}
