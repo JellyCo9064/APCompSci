ca326334fa9dd25a96a1759ea4af781b63d02e86
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2017-11-08 10:03:13 -0800

    Actually done

--- 9_2WriteSquares/src/main.java
+++ 9_2WriteSquares/src/main.java
@@ -3,8 +3,8 @@
  * Project Name: 9_2WriteSquares
  * File Name: main.java
  * Purpose: Recursion w/ helpers
- * Pseudocode: Pass n to initial method, pass nearest even value to helper method (keeps track of iterative number, and initial n value) Repass n - 2 if even, n +2 if odd. End Recursion if less than or equal to ni
- * Maintenance Log: 11/6 Done
+ * Pseudocode: Pass n - 1 to subsequent methods, print out before calling function if even, print out after calling function if odd
+ * Maintenance Log: 11/6 Done? 11/8 DONE!!!
  */
 public class main {
 
@@ -14,59 +14,21 @@ public class main {
 	}
 	public static void writeSquares(int n)//Method with Singular parameter for user ease.
 	{
-		if (n <= 0)
+		if (n == 1)//base case
 		{
-			System.out.println("Invalid");
+			System.out.print("1, ");
 			return;
 		}
-		if(n % 2 == 0)//n is already even
+		if (n % 2 == 0)
 		{
-			writeSquares(n, n);
-			return;
+			System.out.print(n * n + ", ");//Prints out even squares before recursing. Allows for intermittent printing while functions are stacking up
+			writeSquares(n - 1);
 		}
-		else//n is odd, seed recursion with even
+		else
 		{
-			writeSquares(n - 1, n);
-			return;
+			writeSquares(n - 1);//Printing has to wait until base case is reached
+			System.out.print(n * n + ", ");
 		}
-	}
-	private static void writeSquares(int nf, int ni)
-	{
-		if (nf < 1)//Switches from decrementing, to incrementing
-		{
-			writeSquares(1, ni);
-		}						
-		else if(nf % 2 == 0)
-		{
-			writeSquares(nf - 2, ni);//Moves down n
-			System.out.print(nf * nf);
-			if (nf != ni && nf + 1 != ni)//Prints out commas, only if final even square is not being printed
-			{
-				System.out.print(", ");
-			}
-			else
-			{
-				return;
-			}
-		}
-		else if(nf % 2 == 1)
-		{
-			if(nf <= ni)
-			{
-				writeSquares(nf + 2, ni);//Moves up to next odd
-				if (ni != 1)
-				{
-					System.out.print(nf * nf + ", ");
-				}
-				else
-				{
-					System.out.print(1);//No need for comma if only "1"
-				}
-			}
-			else
-			{
-				return;
-			}
-		}
+		
 	}
 }
