ddb71912709b20a053632a72e0f7accac76db5c9
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2018-02-07 10:43:40 -0800

    Implemented Binary Search

--- AP11_1Dictionary/src/main.java
+++ AP11_1Dictionary/src/main.java
@@ -4,7 +4,7 @@
  * File Name: main.java
  * Purpose: Sort and Search
  * Pseudocode: Read in file, implement collection.sort and collection.binarysearch, subtract indexes
- * Maintenance Log: 1/31 Done
+ * Maintenance Log: 1/31 Done (lol jk) 2/7 Implemented binary search
  */
 import java.util.*;
 import java.io.*;
@@ -33,9 +33,9 @@ public class main {
 		System.out.println("Enter second word: ");
 		String second = console.nextLine();
 		
-		int firstIndex = Collections.binarySearch(dictionary, first, String.CASE_INSENSITIVE_ORDER);//Search for words
+		int firstIndex = binarySearch(dictionary, first);//Search for words
 		
-		int secondIndex = Collections.binarySearch(dictionary, second, String.CASE_INSENSITIVE_ORDER);
+		int secondIndex = binarySearch(dictionary, second);
 		
 		if (firstIndex < 0)//Not found
 		{
@@ -50,6 +50,34 @@ public class main {
 			System.out.println("There are " + (Math.abs(secondIndex - firstIndex) - 1) + " between " + first + " and " + second);
 		}
 		console.close();
+	}
+	public static int binarySearch(List<String> dictionary, String target)
+	{
+		if(dictionary.size() <= 1)
+		{
+			if(dictionary.get(0).compareToIgnoreCase(target) == 0)
+				return 0;//Word Found
+			else
+				throw new IllegalArgumentException("Word Not Found");
+		}
+		int compare = dictionary.get(dictionary.size() / 2).compareToIgnoreCase(target);
+		if(compare < 0)//Target comes after partition
+		{
+			return (int)Math.ceil((double)dictionary.size() / 2) + binarySearch(dictionary.subList(dictionary.size() / 2 + 1, dictionary.size()), target);
+			//Return number of indexes before partition, recursion with sub list
+		}
+		else if(compare > 0)
+		{
+			return binarySearch(dictionary.subList(0, dictionary.size() / 2), target);
+			//recursion with sub list
+		}
+		else
+		{
+			return (int)(dictionary.size() / 2);
+			//Index of target found
+		}
+		
+
 	}
 
 }
