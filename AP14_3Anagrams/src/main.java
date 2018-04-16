49cb3e1d1ac533110ec448595a6650d36ccbcb55
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2018-04-16 10:37:57 -0700

    Fix add to map

--- AP14_3Anagrams/src/main.java
+++ AP14_3Anagrams/src/main.java
@@ -10,14 +10,45 @@ public class main {
 		
 		loadDictionary(dictionary, canonicalDictionary);
 		
+		System.out.println(canonicalDictionary.toString());
+		HashMap<String, Set<String>> canonicalToSet = new HashMap<String, Set<String>>();
+		
+		String previous = "";
+		for(int i = 0; i < dictionary.size(); i++)
+		{
+			if (canonicalDictionary.get(i) != previous)
+			{
+				previous = canonicalDictionary.get(i);
+				canonicalToSet.put(previous, new HashSet<String>());
+				canonicalToSet.get(previous).add(dictionary.get(i));
+			}
+			else
+			{
+				System.out.println("yay");
+				canonicalToSet.get(previous).add(dictionary.get(i));
+			}
+		}
+		
+		
+		
 		Scanner console = new Scanner(System.in);
 		
 		System.out.println("Enter String: ");
 		String target = console.nextLine();
-		
-		int index = Collections.binarySearch(canonicalDictionary, target);
-		//TODO: Find bounds of canonical
-		ListIterator<String> itr = canonicalDictionary.listIterator();
+		target = findCanonical(target);
+		Set<String> words = canonicalToSet.get(target);
+		System.out.println(canonicalToSet.get(target).toString());
+		if(words.size() != 0)
+		{
+			for(String s : words)
+			{
+				System.out.println(s);
+			}
+		}
+		else
+		{
+			System.out.println("");
+		}
 
 	}
 	public static void loadDictionary(List<String> dictionary, List<String> canonicalDictionary) 
@@ -38,6 +69,7 @@ public class main {
 			canonicalDictionary.add(findCanonical(line));
 		}
 		Collections.sort(dictionary, new COMPARE_CANONICAL());
+		Collections.sort(canonicalDictionary);
 	}
 	public static String findCanonical(String str)
 	{
