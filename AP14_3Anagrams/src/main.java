6184a3f0a891ebc2caed26b8226969f9b19d5871
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2018-04-18 10:51:32 -0700

    All Done

--- AP14_3Anagrams/src/main.java
+++ AP14_3Anagrams/src/main.java
@@ -1,4 +1,11 @@
-
+/*
+ * Author: Connor Aksama
+ * Project Name: AP14_3Anagrams
+ * File Name: main.java
+ * Purpose: Lots O Map
+ * Pseudocode: Load words into list, sort by canonical form, load canonical forms into list. Load into map Key: canonical form Value: words with same canonical form. Given target by user -> access value in map
+ * Maintenance Log: 4/18 done haha.
+ */
 import java.util.*;
 import java.io.*;
 public class main {
@@ -8,23 +15,21 @@ public class main {
 		List<String> dictionary = new ArrayList<String>();
 		List<String> canonicalDictionary = new ArrayList<String>();
 		
-		loadDictionary(dictionary, canonicalDictionary);
+		loadDictionary(dictionary, canonicalDictionary);//Fill lists
 		
-		System.out.println(canonicalDictionary.toString());
-		HashMap<String, Set<String>> canonicalToSet = new HashMap<String, Set<String>>();
+		HashMap<String, Set<String>> canonicalToSet = new HashMap<String, Set<String>>();//Key: Canonical form of word Value: Set of words with Key as its canonical form
 		
 		String previous = "";
 		for(int i = 0; i < dictionary.size(); i++)
 		{
-			if (canonicalDictionary.get(i) != previous)
+			if (!canonicalDictionary.get(i).equals(previous))//If new canonical form reached
 			{
 				previous = canonicalDictionary.get(i);
-				canonicalToSet.put(previous, new HashSet<String>());
-				canonicalToSet.get(previous).add(dictionary.get(i));
+				canonicalToSet.put(previous, new HashSet<String>());//Add new Key
+				canonicalToSet.get(previous).add(dictionary.get(i));//Add word to set
 			}
-			else
+			else//Canonical form exists
 			{
-				System.out.println("yay");
 				canonicalToSet.get(previous).add(dictionary.get(i));
 			}
 		}
@@ -35,25 +40,26 @@ public class main {
 		
 		System.out.println("Enter String: ");
 		String target = console.nextLine();
-		target = findCanonical(target);
-		Set<String> words = canonicalToSet.get(target);
-		System.out.println(canonicalToSet.get(target).toString());
-		if(words.size() != 0)
+		Set<String> words = canonicalToSet.get(findCanonical(target));//Access Set Value
+		if(words.size() != 1)
 		{
 			for(String s : words)
 			{
-				System.out.println(s);
+				if(!s.equals(target))
+				{
+					System.out.println(s);//Print words
+				}
 			}
 		}
 		else
 		{
-			System.out.println("");
+			System.out.println("No anagrams found. RIP");
 		}
 
 	}
 	public static void loadDictionary(List<String> dictionary, List<String> canonicalDictionary) 
 	{
-		Scanner file = null;
+		Scanner file = null;//Load file
 		try
 		{
 			file = new Scanner(new File("dictionary.txt"));
@@ -64,16 +70,16 @@ public class main {
 		}
 		while(file.hasNextLine())
 		{
-			String line = file.nextLine();
+			String line = file.nextLine();//Add words to list
 			dictionary.add(line);
 			canonicalDictionary.add(findCanonical(line));
 		}
-		Collections.sort(dictionary, new COMPARE_CANONICAL());
+		Collections.sort(dictionary, new COMPARE_CANONICAL());//Sort by canonical form
 		Collections.sort(canonicalDictionary);
 	}
 	public static String findCanonical(String str)
 	{
-		char[] charray = str.toCharArray();
+		char[] charray = str.toCharArray();//Sorts characters
 		Arrays.sort(charray);
 		return new String(charray);
 	}
