bf50164cc941625fe1572cfec452b49a1fe42643
aksamac <aksamac@V10RM126W10-14.rsd407.org>
2018-04-25 10:03:17 -0700

    Distance of 0

--- AP14_1WordDistance/src/main.java
+++ AP14_1WordDistance/src/main.java
@@ -4,7 +4,7 @@
  * File Name: main.java
  * Purpose: Maps?
  * Pseudocode: wubabababubuabuaubaubauba
- * Maintenance Log: 
+ * Maintenance Log: 4/19 Done
  */
 import java.util.*;
 import java.io.*;
@@ -27,9 +27,9 @@ public class main {
 		{
 			System.out.println("Word Lengths Do Not Match");
 		}
-		loadDictionary(words, one.length());
+		loadDictionary(words, one.length());//Load Dictionary
 		boolean isWord = true;
-		if(!words.contains(one))
+		if(!words.contains(one))//Check if valid word
 		{
 			System.out.println(one + " is not a word.");
 			isWord = false;
@@ -39,28 +39,44 @@ public class main {
 			System.out.println(two + " is not a word.");
 			isWord = false;
 		}
-		if(isWord)
+		boolean path = true;
+		if(isWord && !one.equals(two))
 		{
 			editDistance = 1;
-			constructMap(wordMap, words);
-			Set<String> searchList = new HashSet<String>();
-			searchList.addAll(wordMap.get(one));
-			for(int i = 0; i < 1000; i++)
+			constructMap(wordMap, words);//Load Map
+			Set<String> searchList = new HashSet<String>();//Current set of words to be searched
+			Set<String> superSet = new HashSet<String>();//Set of all words searched
+			searchList.addAll(wordMap.get(one));//Add words with distance of one from target word
+			superSet.addAll(wordMap.get(one));
+			for(int i = 0; i < 10000; i++)
 			{
-				if(searchList.contains(two))
+				if(searchList.contains(two))//Edit distance of 1
 				{
 					break;
 				}
-				Set<String> toAdd = new HashSet<String>();
+				Set<String> toAdd = new HashSet<String>();//Words to add to searchList
 				for(String s : searchList)
 				{
 					toAdd.addAll(wordMap.get(s));
 				}
 				searchList.clear();
 				searchList.addAll(toAdd);
+				if(superSet.containsAll(searchList))//All words to be searched have already been searched -> no path
+				{
+					path = false;
+					break;
+				}
+				superSet.addAll(toAdd);
 				editDistance++;
 			}
+		}
+		if (path)
+		{
 			System.out.println("Edit Distance Of: " + editDistance);
+		}
+		else
+		{
+			System.out.println("Path not found....");
 		}
 		
 	}
@@ -75,10 +91,10 @@ public class main {
 		{
 			System.out.println("File Not Found");
 		}
-		while(file.hasNextLine())
+		while(file.hasNextLine())//Load lines into dictionary
 		{
 			String line = file.nextLine();
-			if(line.length() == wordLength)
+			if(line.length() == wordLength)//Add lines of same length
 			{
 				words.add(line);
 			}
@@ -87,28 +103,28 @@ public class main {
 	public static void constructMap(Map<String, Set<String>> wordMap, Set<String> words)
 	{
 
-		for(String word : words)
+		for(String word : words)//For every word of given length
 		{
 			Set<String> editOne = new HashSet<String>();
 			
-			for(int i = 0; i < word.length(); i++)
+			for(int i = 0; i < word.length(); i++)//Iterate through each position
 			{
-				for(char c = 'a'; c <= 'z'; c++)
+				for(char c = 'a'; c <= 'z'; c++)//Iterate through each character
 				{
 					if(c != word.charAt(i))
 					{
 						char[] wordArray = word.toCharArray();
-						wordArray[i] = c;
+						wordArray[i] = c;//Replace character
 						String newWord = new String(wordArray);
-						if(words.contains(newWord))
+						if(words.contains(newWord))//Edited word exists
 						{
-							editOne.add(newWord);
+							editOne.add(newWord);//Add to set
 						}
 					}
 					
 				}
 			}
-			wordMap.put(word, editOne);
+			wordMap.put(word, editOne);//Add set to map as value with current word as Key
 		}
 		
 	}
